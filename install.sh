67978be0251d0aba830460faa8e2d1d37c1fdf5e
Rafael Rossi <91340426+rafaellrossi@users.noreply.github.com>
2024-08-23 17:13:20 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install curl pwgen -y
+curl -fsSL https://code-server.dev/install.sh | sh
+export PASSWORD=$(pwgen 16 1)
+code-server --port 10000 --host 0.0.0.0 --disable-telemetry
+
+echo ""
+echo "Vscode-server configurado com sucesso."
+echo "Acessar Vscode-server em http://<IP ou URL>:10000"
+echo "Senha: $PASSWORD"
+echo ""
